228577e09aa949769369451ae38489427bc8bc80
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-09-26 12:15:50 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'chats#index'
+  root 'groups#index'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :articles
-  resources :users,only: [:edit,:update]
+
+  resources :users,only: [:index,:edit,:update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
