ad1939fe99f8e6bff84b4a50c0687ac349156f71
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-09-28 17:13:08 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :users,only: [:index,:edit,:update]
+  resource :user,only: [:edit,:update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index,:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
