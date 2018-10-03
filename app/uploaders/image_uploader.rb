e115d2cd15baf5b8d167a914f4a199ba0c8ea091
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-10-03 16:33:45 +0900

    rails g uploader imageをやった

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
-  process resize_to_fit: [800, 800]
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
