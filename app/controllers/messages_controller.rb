26a53f08fc77ac002ee53763f83c935cd1632d39
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-10-03 16:33:31 +0900

    bingingpryをコメントアウト

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+    # binding.pry
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
