6a580f17322ed6014a43b300a4c8e604c062ebdb
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-10-10 16:53:15 +0900

    コンフリクト部分の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function(){
   function buildHTML(message){
+    var image = message.image ? `<img src = '${message.image}' width="256" height="256">` : '';
     var html = `
           <div class="chat-box">
               <div class="nameday-box">
@@ -34,8 +35,9 @@ $(function(){
       $('.right-mainbody').append(html)
       $('.form__submit').prop('disabled', false);
       $('#message_text').val('');
+      $('#message_image').val('');
       $messages = $('.right-mainbody');
-      $messages.animate({scrollTop: $messages[0].scrollHeight}, 10000);
+      $messages.animate({scrollTop: $messages[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('error');
