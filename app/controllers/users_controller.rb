badbd1161ba02a50041b21808b43189dae0439b2
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-10-11 20:49:06 +0900

    controllerであいまい検索設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
       end
   end
 
+  def index
+    #currentユーザーは取り除く
+    @users =User.where('name LIKE(?) or id != ?', "%#{params[:keyword]}%",current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:mail,:name)
