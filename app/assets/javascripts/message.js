cf7fd2887f5962e176367d7746635e9d4fcb5bff
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-10-14 15:02:24 +0900

    message.jsに自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,11 @@
+
+
 $(document).on('turbolinks:load', function() {
+    // setInterval(function() {
   function buildHTML(message){
     var image = message.image ? `<img src = '${message.image}' width="256" height="256">` : '';
     var html = `
-          <div class="chat-box">
+          <div class="chat-box" id="${message.id}">
               <div class="nameday-box">
                 <p class="chat-name">
                   ${message.user_name}
@@ -38,7 +41,7 @@ $(document).on('turbolinks:load', function() {
       $messages.animate({scrollTop: $messages[0].scrollHeight}, 'fast');
     })
     .fail(function(data){
-      alert('error');
+      // alert('error');
       var html = buildHTML(data);
     })
     .always(function(data){
@@ -46,4 +49,52 @@ $(document).on('turbolinks:load', function() {
       $('.form__submit').prop('disabled', false);
     })
   })
+
+// ここから下が自動更新
+var interval = setInterval(function(){
+    // setIntervalで5秒ごとに更新
+     var presentMessageId = $('.chat-box').last().attr('id')
+     // console.log(presentMessageId)
+     // 最新のメッセージからidを取得
+    var presentHTML = window.location.href
+    // console.log(presentHTML)
+    // 現在のURLを表示
+
+    // ↓メッセージ画面以外だと反応しないようにする
+     if (presentHTML.match(/\/groups\/\d+\/messages/)) {
+      console.log(presentHTML)
+    // 現在のURLからIDを取得
+       $.ajax ({
+        url: presentHTML,
+        type: 'GET',
+        data: {id: presentMessageId},
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+       .done(function(json){
+        // insertHTMLを空欄として定義する
+        var insertHTML = "";
+
+        json.forEach(function(message){
+          if (message.id > presentMessageId){
+        // insertHTMLにbuildHTMLを加える（messageの中身は変数）また、buildHTMLは非同期のつぶやき内容
+            insertHTML += buildHTML(message);
+            // cosole.log(insertHTML)
+        // 右側のright-mainbodyに非同期のつぶやきであるbuildHTMLを入れていく作業
+            $messages = $('.right-mainbody');
+            $messages.append(insertHTML);
+            $messages.animate({scrollTop: $messages[0].scrollHeight}, 'fast');
+            // alert("success")
+          }
+        });
+      })
+       .fail(function(data){
+        // alert('error')
+      });
+     } else {
+      clearInterval(interval)
+    }
+  },5000);
 });
+
