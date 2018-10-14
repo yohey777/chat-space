fe217122c91e0e29492790991aad9b57db397878
yohei okamoto <yo.world.traveler.123@gmail.com>
2018-10-14 15:12:09 +0900

    message_controllerの不要なコメントアウトの削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,6 @@ class MessagesController < ApplicationController
     respond_to do |format|
       format.html
       format.json { @new_messages = @messages.where('id > ?', params[:id])}
-      # binding.pry
     end
   end
 
